5e5f855ba00e2c96e582133162c1ad65515abc87
Jason Moon <jasonmoonjm@gmail.com>
2019-10-14 14:45:00 -0700

    create magic 8 ball expression

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.get('/greeting', (req, res) => {
     res.send(`Hello Stranger`)
 })
 
+app.get('/magic/:question', (req,res) => {
+    const x = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    res.send(`${req.params.question}: ${x[Math.floor(Math.random() * x.length)]}`)
+})
+
 app.get('/tip/:total/:percentage', (req, res) => {
     const x = req.params.total * (req.params.percentage * .01);
     res.send(`${x}`);
