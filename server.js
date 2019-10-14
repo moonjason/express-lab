dca58dcc4688b5bee883b465199f6a253734ac7f
Jason Moon <jasonmoonjm@gmail.com>
2019-10-14 14:34:18 -0700

    creat tip calculator

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ app.get('/greeting', (req, res) => {
     res.send(`Hello Stranger`)
 })
 
+app.get('/tip/:total/:percentage', (req, res) => {
+    const x = req.params.total * (req.params.percentage * .01);
+    res.send(`${x}`);
+})
+
 app.get('/greeting/:name', (req, res) => {
-    res.send(`Hello ${req.params.name}`)
+    res.send(`Wow! Hello there ${req.params.name}!`)
 })
 
 app.listen(6969, (err) => {
     console.log('I am running on port 6969');
 });
+
